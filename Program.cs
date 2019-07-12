3c79d3f9a6ccff54d43b4dd01c3a6ff6b44d19c0
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2019-07-11 22:56:12 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using prmToolkit.Selenium;
+using prmToolkit.Selenium.Enum;
+
+namespace BotInstagramSelenium
+{
+    class Program
+    {
+        private static string senha = "";
+        static void Main(string[] args)
+        {
+            IWebDriver webDriver = WebDriverFactory.CreateWebDriver(Browser.Chrome,"/Users/GuiGolfetto/Desktop/study/cursoWebCrawler/selenium/");
+
+            try
+            {
+                webDriver.LoadPage(TimeSpan.FromSeconds(10), "https://www.instagram.com/accounts/login");
+
+                webDriver.SetText(By.Name("username"),"");
+                webDriver.SetText(By.Name("password"),senha);
+
+                webDriver.Submit(By.TagName("button"));
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                webDriver.LoadPage(TimeSpan.FromSeconds(10), "https://www.instagram.com/caitomaiaoficial");
+
+                webDriver.FindElement(By.XPath("//*[@id='react-root']/section/main/div/header/section/div[1]/div[1]/span/span[1]/button")).Click();
+
+
+            } catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message.ToString());
+            } 
+            finally
+            {
+                //webDriver.Close();
+                //webDriver.Dispose();
+            }
+            
+        }
+    }
+}
